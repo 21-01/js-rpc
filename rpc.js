2785bfcd6487c4f8b08cb9b0b290ede3e85824ed
21-01 <banyaklontelontemaudijual+git@gmail.com>
2023-11-08 03:31:08 +0700

    add header dummy

--- rpc.js
+++ rpc.js
@@ -29,32 +29,31 @@ function startRound(){
     }
 }
 
-function bo5Game(){
-    var computerScores = 0;
-    var playerScores = 0;
-    while (true){
-        console.log("Computer Scores:", computerScores)
-        console.log("Player Scores:", playerScores)
-        roundWinner = startRound();
-        if (roundWinner == "Computer Win"){
-            console.log(roundWinner)
-            computerScores++
-        }
-        else if (roundWinner == "Player Win"){
-            console.log(roundWinner)
-            playerScores++
-        }
-        else{
-            console.log(roundWinner)
-        }
+// Obselete function
+// function bo5Game(){
+//     var computerScores = 0;
+//     var playerScores = 0;
+//     while (true){
+//         console.log("Computer Scores:", computerScores)
+//         console.log("Player Scores:", playerScores)
+//         roundWinner = startRound();
+//         if (roundWinner == "Computer Win"){
+//             console.log(roundWinner)
+//             computerScores++
+//         }
+//         else if (roundWinner == "Player Win"){
+//             console.log(roundWinner)
+//             playerScores++
+//         }
+//         else{
+//             console.log(roundWinner)
+//         }
 
-        if (computerScores == 5){
-            return "Computer Win The Match"
-        }
-        else if (playerScores == 5){
-            return "Player Win The Match"
-        }
-    }
-}
-
-console.log(bo5Game())
+//         if (computerScores == 5){
+//             return "Computer Win The Match"
+//         }
+//         else if (playerScores == 5){
+//             return "Player Win The Match"
+//         }
+//     }
+// }
